74ac7df1c4e88f2c9ff09361faed3dd1c9930c37
MonicaMalla85 <inychan85@gmail.com>
2025-11-07 14:48:35 +0100

    index done

---
+++ ListaUtenti/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rubrica Contatti</title>
+</head>
+<body>
+
+
+        <h1>Rubrica Contatti</h1>
+
+        <h2>Aggiungi Contatto</h2>
+
+        <form action="" method="POST">
+
+            Nome : <input type="text" name="name">
+            Telefono : <input type="text" name="phone">
+
+            <button type="submit" name="add">Aggiungi</button>
+
+        </form>
+
+
+
+        <h2>Ricerca Contatto</h2>
+        
+        <form action="" method="POST">
+
+            Nome : <input type="text" name="search_name">
+
+            <button type="submit" name="search">Cerca</button>
+
+        </form>
+
+
+
+
+
+
+</body>
+</html>
