ff06a0affb9b4de542cf51868a3a88b0642e2371
MonicaMalla85 <inychan85@gmail.com>
2025-11-07 14:41:23 +0100

    addContact done

---
+++ ListaUtenti/Contact.php
@@ -0,0 +1,26 @@
+<?php
+    class Contact {
+
+        //Attributi
+        public string $name;
+        public string $phone;
+
+        //Costruttore
+        public function __construct(string $name, string $phone) {
+            $this->name = $name;
+            $this->phone = $phone;
+        }
+
+        //Metodo o funzione
+        public function getInfo(): string {
+
+           return "Nome : $this->name - Telefono $this->phone"; 
+        }
+
+
+    }
+
+
+
+
+?>
